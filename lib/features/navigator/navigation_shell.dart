40cc06e8cd2a9aff2551348dc2c356ff1a0a6204
amira_nasser <amira20nasser@gmail.com>
2025-10-08 12:27:32 +0300

    move the navigator to features folder

--- lib/features/navigator/navigation_shell.dart
+++ lib/features/navigator/navigation_shell.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
-import '../../core/constants/app_colors.dart';
+import '../../../core/constants/app_colors.dart';
 
 class NavigationShell extends StatelessWidget {
   final StatefulNavigationShell navigationShell;
